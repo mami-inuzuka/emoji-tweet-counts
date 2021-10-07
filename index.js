41a500428927aad4654dbb48b6b1e2007067db7a
mami-inuzuka <inuzuka.mami@gmail.com>
2021-10-07 21:19:48 +0900

    promptsライブラリを用いて外から絵文字や対象のアカウントを設定できるようにする

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const needle = require('needle')
 const { DateTime } = require('luxon')
+const prompts = require('prompts')
 
 const token = process.env.BEARER_TOKEN
 const endpointUrl = 'https://api.twitter.com/2/tweets/counts/recent'
 
-const targetUserName = 'TwitterJP'
-const emoji = '🐥'
+let targetUserName = ''
+let emoji = ''
+
+function main () {
+  console.log('\n')
+  console.log('ーーーーーーーーーーーーーーーーーーーーーーーーーーーー')
+  console.log('| 　　　　　　　　　　　　　　　　　　　　　　　　　　 |')
+  console.log('|　　🐥直近一週間のツイート数を絵文字で表示します🐥　　|')
+  console.log('| 　　　　　　　　　　　　　　　　　　　　　　　　　　 |')
+  console.log('ーーーーーーーーーーーーーーーーーーーーーーーーーーーー')
+  console.log('\n')
+
+  getInfo()
+}
 
 async function getRequest () {
   const params = {
@@ -28,7 +41,7 @@ async function getRequest () {
 async function displayTweetCounts () {
   try {
     const response = await getRequest()
-    console.log('\n' + '@' + targetUserName + 'のツイート数' + '\n')
+    console.log('\n' + '@' + targetUserName + ' の直近一週間のツイート数' + '\n')
     for (let i = 0; i < response.data.length; i++) {
       const tweetCounts = response.data[i].tweet_count
       const date = DateTime.fromISO(response.data[i].start).setLocale('ja').toISODate()
@@ -36,6 +49,7 @@ async function displayTweetCounts () {
       process.stdout.write(date + ' ')
       console.log(tweetCountsSign)
     }
+    console.log('\n')
   } catch (e) {
     console.log(e)
     process.exit(-1)
@@ -43,4 +57,23 @@ async function displayTweetCounts () {
   process.exit()
 }
 
-displayTweetCounts()
+async function getInfo () {
+  const question = [
+    {
+      type: 'text',
+      name: 'emoji',
+      message: '好きな絵文字を入力してね'
+    },
+    {
+      type: 'text',
+      name: 'targetUserName',
+      message: '表示したいアカウント名を入力してね（@は不要です）'
+    }
+  ]
+  const response = await prompts(question)
+  emoji = response.emoji
+  targetUserName = response.targetUserName
+  displayTweetCounts()
+}
+
+main()
